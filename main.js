ddecbb1119e498518b4cd0d425815055888fb916
Sandeep <sandeep-kainth@hotmail.co.uk>
2018-04-28 00:49:26 +0100

    adding connections to firebase p1

---
+++ main.js
@@ -0,0 +1,41 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDSBy30StN-Wuw6sk2nKpPaGF4KADyzQK4",
+  authDomain: "final-year-blog-project.firebaseapp.com",
+  databaseURL: "https://final-year-blog-project.firebaseio.com",
+  projectId: "final-year-blog-project",
+  storageBucket: "final-year-blog-project.appspot.com",
+  messagingSenderId: "293490381660"
+};
+firebase.initializeApp(config);
+
+var messagesRef = firebase.database().ref('messages');
+
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+function submitForm(e){
+  e.preventDefault();
+
+  var name = getInputVal('name');
+  var username = getInputVal('username');
+  var email = getInputVal('email');
+  var message = getInputVal('message');
+
+  saveMessage(name, username, email, message);
+
+
+}
+
+function getInputVal(id){
+  return document.getElementbyId(id).value;
+}
+
+function saveMessage(name, username, email, message) {
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    username: username,
+    email : email,
+    message : message
+  });
+}
